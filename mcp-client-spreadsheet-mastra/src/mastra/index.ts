fd240578aab7f13ea12a670d2c0902a0a515304a
Hitomi Uchihi <pecobitaro@gmail.com>
2025-05-09 11:56:58 +0900

    サンプルのMCPを定義

--- mcp-client-spreadsheet-mastra/src/mastra/index.ts
+++ mcp-client-spreadsheet-mastra/src/mastra/index.ts
@@ -3,15 +3,22 @@ import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { LibSQLStore } from '@mastra/libsql';
 import { weatherWorkflow } from './workflows';
-import { weatherAgent } from './agents';
+import { multiAgent } from './agents';
 
 export const mastra = new Mastra({
+  // 使用可能なワークフロー
   workflows: { weatherWorkflow },
-  agents: { weatherAgent },
+
+  // 使用可能なエージェント
+  agents: { multiAgent },
+
+  // ストレージ: LibSQLStoreを使用して、SQLiteデータベースに保存
   storage: new LibSQLStore({
     // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
     url: ":memory:",
   }),
+
+  // ログ
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
